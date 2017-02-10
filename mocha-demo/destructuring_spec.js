3a592ebeed50392b1a866614806bc44969d498da
Roman Nagorkin <roman.nagorkin@gmail.com>
2017-02-10 18:40:12 -0500

    Added destructuring usage sample in functions

---
+++ mocha-demo/destructuring_spec.js
@@ -0,0 +1,19 @@
+function writeName({ name, age }){
+    console.log(name);
+    console.log(age);
+}
+
+const user = {
+    name: 'dave',
+    gender: 'male',
+    age: '24'
+};
+
+
+describe('Destructuring', () => {
+    require('should');
+    it('can be used to specify object\'s interface in function arguments', () => {
+        writeName(user);
+        true.should.be.exactly(true);
+    });
+});
