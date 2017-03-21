6cd50c5181e0ee63d10b9c70f20cdc716f797806
Roman Nagorkin <roman.nagorkin@gmail.com>
2017-03-21 11:10:50 -0400

    Added max, sum

--- mocha-demo/recursion-spec.js
+++ mocha-demo/recursion-spec.js
@@ -7,10 +7,40 @@ var countLength = function countLength(arr) {
     return countLengthFor(arr, 0);
 };
 
-describe('recLength rec', () => {
+var sum = function sum(arr) {
+    function sumFor(arr, accum) {
+        var item = arr.slice(0, 1)[0];
+        return item === undefined
+            ? accum
+            : sumFor(arr.slice(1), accum + item);
+    }
+    return sumFor(arr, 0);
+};
+
+var max = function max(arr) {
+    function maxFor(arr, accum) {
+        var item = arr.slice(0, 1)[0];
+        return item === undefined
+            ? accum
+            : maxFor(arr.slice(1), item > accum ? item : accum);
+    }
+    return maxFor(arr, -Infinity);
+};
+
+describe('recursion', () => {
     require('should');
     it('can count length', () => {
         countLength([4, 5, 6, 7]).should.be.exactly(4);
         countLength([]).should.be.exactly(0);
+    });
+
+    it('can count sum', () => {
+        sum([4, 5, 6, 7]).should.be.exactly(22);
+        sum([]).should.be.exactly(0);
+    });
+
+    it('can count max', () => {
+        max([4, 5, 6, 7, 3, 2, 1, 7, 4]).should.be.exactly(7);
+        max([]).should.be.exactly(-Infinity);
     });
 });
